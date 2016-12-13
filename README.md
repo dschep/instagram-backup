3a43f13ef49c68dc61b106ab8c939cd41eced24f
Daniel Schep <dschep@gmail.com>
2016-12-12 20:36:22 -0500

    :tada:

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instagram to S3 backup
+[![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
+
+```
+# edit serverless.yml to set env vars
+npm install
+sls deploy
+```
---
+++ handler.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const InstagramPosts = require('instagram-screen-scrape').InstagramPosts;
+const Promise = require('bluebird');
+const streamToPromise = require('stream-to-promise');
+const fetch = require('node-fetch')
+
+const AWS = require('aws-sdk');
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const s3 = new AWS.S3();
+
+
+module.exports.instagramToS3 = (event, context, callback) => {
+  var bucketContents;
+  s3.listObjects({Bucket: process.env.s3Bucket}).promise()
+    .then((resp) => {
+      bucketContents = resp.Contents;
+      return streamToPromise(new InstagramPosts({
+        username: process.env.screenName
+      }));
+    })
+    .then((posts) => Promise.filter(posts, (post) => Boolean(bucketContents.indexOf(`${post.id}.jpg`))))
+    .then((posts) => Promise.map(posts, (post) => {
+      const fileName = post.id + '.jpg';
+      return fetch(post.media)
+        .then((resp) => resp.buffer())
+        .then((buffer) => {
+          return s3.putObject({
+            Bucket: process.env.s3Bucket,
+            Key: fileName,
+            Body: buffer,
+          }).promise();
+        })
+    }))
+    .then((images) => callback(null, {statusCode: 200, body: 'ok'}));
+};
